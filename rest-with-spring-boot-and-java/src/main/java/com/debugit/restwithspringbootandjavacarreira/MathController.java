8420182ac768851affe3915562fed1ef26d43d15
Ewerton Carreira <ewertoncarreira@gmail.com>
2023-04-24 17:23:11 -0300

    Definindo os Métodos de Validação e Executando a Aplicação

--- rest-with-spring-boot-and-java/src/main/java/com/debugit/restwithspringbootandjavacarreira/MathController.java
+++ rest-with-spring-boot-and-java/src/main/java/com/debugit/restwithspringbootandjavacarreira/MathController.java
@@ -1,11 +1,11 @@
 package com.debugit.restwithspringbootandjavacarreira;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class MathController {
@@ -13,14 +13,10 @@ public class MathController {
     private static final String template = "Hello, %s!";
     private static final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping(
-            value = "/sum/{numberOne}/{numberTwo}",
-            method = RequestMethod.GET
-    )
+    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
-            @PathVariable(value = "numberTwo") String numberTwo
-    ) throws Exception {
+            @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
 
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
             throw new Exception();
@@ -29,11 +25,24 @@ public class MathController {
         return converToDouble(numberOne) + converToDouble(numberTwo);
     }
 
-    private Double converToDouble(String numberOne) {
+    private Double converToDouble(String strNumber) {
+        if (strNumber == null)
+            return 0D;
+
+        String number = strNumber.replaceAll(",", ".");
+
+        if (isNumeric(number))
+            return Double.parseDouble(number);
+
         return null;
     }
 
-    private boolean isNumeric(String numberOne) {
-        return false;
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null)
+            return false;
+
+        String number = strNumber.replaceAll(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 }
